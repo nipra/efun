377fde56a750ab77917557dea662bf27d191d51d
Nikhil Prabhakar <nipra@infinitelybeta.com>
2011-07-25 13:06:57 +0530

    * Changes.

--- tr_server.erl
+++ tr_server.erl
@@ -1,128 +1,132 @@
 %%%-------------------------------------------------------------------
+%%% File    : tr_server.erl
 %%% @author Nikhil Prabhakar <prabhakar.nikhil@gmail.com>
-%%% @copyright (C) 2011, Nikhil Prabhakar
-%%% @doc RPC over TCP server. This module defines a server process that
-%%%      listens for incoming TCP connections and allows the user to
-%%%      execute RPC commands via that TCP stream.
+%%% @copyright 2011 Nikhil Prabhakar
+%%% @doc RPC over TCP server. This module defines a server process
+%%%      that listens for incoming TCP connections and allows the
+%%%      user to execute RPC commands via that TCP stream.
+%%%
+%%% Created : 22 Jul 2011 by Nikhil Prabhakar <prabhakar.nikhil@gmail.com>
 %%% @end
-%%% Created : 18 Jul 2011 by Nikhil Prabhakar <prabhakar.nikhil@gmail.com>
 %%%-------------------------------------------------------------------
 -module(tr_server).
 
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_link/1, get_count/0, stop/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE). 
+-define(SERVER, ?MODULE).
+-define(DEFAULT_PORT, 1055).
 
--record(state, {}).
+-record(state, {port, lsock, request_count = 0}).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
+%%====================================================================
+%% API
+%%====================================================================
 
 %%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @doc Starts the server.
+%% 
+%% @spec start_link(Port::integer()) -> {ok, Pid}
+%% where
+%%  Pid = pid()
 %% @end
 %%--------------------------------------------------------------------
+start_link(Port) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
+
+%% @spec start_link() -> {ok, Pid}
+%% @doc Calls `start_link(Port)' using the default port.
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    start_link(?DEFAULT_PORT).
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
+%% @doc Fetches the number of requests made to this server.
+%% @spec get_count() -> {ok, Count}
+%% where
+%%  Count = integer()
 %% @end
+%%--------------------------------------------------------------------
+get_count() ->
+    gen_server:call(?SERVER, get_count).
+
+
+%%--------------------------------------------------------------------
+%% @doc Stops the server.
+%% @spec stop() -> ok
+%% @end
+%%--------------------------------------------------------------------
+stop() ->
+    gen_server:cast(?SERVER, stop).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
 %%--------------------------------------------------------------------
 init([]) ->
     {ok, #state{}}.
 
 %%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
 %%--------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
 %%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
 %%--------------------------------------------------------------------
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
 %%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
 %%--------------------------------------------------------------------
 handle_info(_Info, State) ->
     {noreply, State}.
 
 %%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
 %%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
     ok.
 
 %%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-%%%===================================================================
+%%--------------------------------------------------------------------
 %%% Internal functions
-%%%===================================================================
+%%--------------------------------------------------------------------
